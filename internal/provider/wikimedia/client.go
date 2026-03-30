db75d52e00393aaf6dab9d7b982c3e252db1928d
Kader <kaderkaaya7@gmail.com>
2026-03-30 22:43:52 +0300

    fix

--- internal/provider/wikimedia/client.go
+++ internal/provider/wikimedia/client.go
@@ -1,1 +1,29 @@
 package wikimedia
+
+import (
+	"net/http"
+	"os"
+	"time"
+)
+
+type Client struct {
+	WikimediaBaseURL string
+	HTTPClient       *http.Client
+	UserAgent        string
+}
+
+func NewClient(baseURL string, timeout time.Duration, userAgent string) *Client {
+	return &Client{
+		WikimediaBaseURL: os.Getenv("WIKIMEDIA_BASE_URL"),
+		UserAgent:        "history-hub", //bazi apiler isteğin kimden geldiğine bkar bundan dolayı bunu ekliyoruz.
+		//eğer bir saldırı olursa engeller ama bunu eklediğimizde bizden geldiği belli olur.
+		HTTPClient: &http.Client{Timeout: timeout},
+		//burda request attığımızda api cevap vermezse sürekli bekler ama
+		//timeout verdiğimizde timeout ile sistemi durdurur.
+		//böylece server kilitlenmez ve kullanıcı beklemez.
+		//tekrar kullanılır
+		//tek yerden yönetilir
+		// tüm requestler aynı ayarla çalışır
+		//http.Client içindeki Timeout alanına fonksiyondan gelen timeout değerini ata
+	}
+}
