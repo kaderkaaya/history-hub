8f82a434dddddddc5be6fce572033464986ead57
Kader <kaderkaaya7@gmail.com>
2026-04-01 22:03:47 +0300

    added models

--- internal/model/event.go
+++ internal/model/event.go
@@ -1,4 +1,8 @@
 package model
 
 type Event struct {
+	Year  int    `json:"year"`
+	Text  string `json:"text"`
+	Title string `json:"title"`
+	URL   string `json:"url"`
 }
--- internal/model/response.go
+++ internal/model/response.go
@@ -1,1 +1,9 @@
 package model
+
+type EventsResponse struct {
+	Date   string  `json:"date"`
+	Lang   string  `json:"lang"`
+	Type   string  `json:"type"`
+	Cached bool    `json:"cached"`
+	Events []Event `json:"events"`
+}
