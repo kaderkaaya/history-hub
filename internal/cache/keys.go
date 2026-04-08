b308ee9a0a60452873b562933421eac4a7a6e2ab
Kader <kaderkaaya7@gmail.com>
2026-04-08 21:15:32 +0300

    added redis

--- internal/cache/keys.go
+++ internal/cache/keys.go
@@ -1,1 +1,7 @@
 package cache
+
+import "fmt"
+
+func BuildOnThisDayKey(lang, typ, month, day string) string {
+	return fmt.Sprintf("historyhub:onthisday:%s:%s:%s:%s", lang, typ, month, day)
+}
--- internal/cache/redis.go
+++ internal/cache/redis.go
@@ -1,1 +1,31 @@
 package cache
+
+import (
+	"context"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisClient struct {
+	client *redis.Client
+}
+
+func NewRedisClient(addr, password string, db int) *RedisClient {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: addr, Password: password, DB: db,
+	})
+	return &RedisClient{client: rdb}
+}
+
+func (r *RedisClient) Ping(ctx context.Context) error {
+	return r.client.Ping(ctx).Err()
+}
+
+func (r *RedisClient) Get(ctx context.Context, key string) (string, error) {
+	return r.client.Get(ctx, key).Result()
+}
+
+func (r *RedisClient) Set(ctx context.Context, key string, value string, ttl time.Duration) error {
+	return r.client.Set(ctx, key, value, ttl).Err()
+}
